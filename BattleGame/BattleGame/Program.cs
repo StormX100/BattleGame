d7965989f3790b3361a76b116de5e4d161845739
StormX100 <radustorm10@gmail.com>
2022-08-25 21:04:06 +0300

    BattleGame-2022: added project files

---
+++ BattleGame/BattleGame/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+using BattleGame.Model;
+using BattleGame.Game;
+
+using System;
+
+namespace BattleGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPlayer firstPlayer = new Warrior() { Health = 50, MaxAttack = 10, MaxBlock = 12 };
+            IPlayer secondPlayer = new Wizard() { Health = 50, MaxAttack = 10, MaxBlock = 12 };
+        }
+    }
+}
