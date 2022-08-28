2d8a59e7a6c52b21ccd48fe68a1250088b8540d4
StormX100 <radustorm10@gmail.com>
2022-08-28 16:17:59 +0300

    BattleGame-2022: moved classes

--- BattleGame/BattleGame/Game/Commander/WarriorCommander.cs
+++ BattleGame/BattleGame/Game/Commander/WarriorCommander.cs
@@ -1,10 +1,6 @@
 ﻿using BattleGame.Game.Options;
 using BattleGame.Model;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BattleGame.Game
 {
